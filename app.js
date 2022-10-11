f1ee6c9d7a755eb7fcf50cc1b387f53f0bf69ebe
aayushi16dex <47508108+aayushi16dex@users.noreply.github.com>
2022-10-11 22:36:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,8 @@ app.get('', (req, res) => {
 })
 
 /** Server */
+const port = process.env.PORT || 3000
 
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log("server started");
 })
